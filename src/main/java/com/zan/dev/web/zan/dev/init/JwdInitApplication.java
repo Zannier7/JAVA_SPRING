3e7cdc171e2f547be75e084cd2bf7c0a11bbf110
zannier7 <zanniervargas@upeu.edu.pe>
2020-07-18 20:14:30 -0500

    INIT: inicializando repositorio for java_spring

---
+++ src/main/java/com/zan/dev/web/zan/dev/init/JwdInitApplication.java
@@ -0,0 +1,13 @@
+package com.zan.dev.web.zan.dev.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwdInitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwdInitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zan/dev/web/zan/dev/init/JwdInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zan.dev.web.zan.dev.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwdInitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
