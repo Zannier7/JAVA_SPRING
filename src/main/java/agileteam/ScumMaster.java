3c61f795cd67d01c7ae175240f1aad09a6db0553
zannier7 <zanniervargas@upeu.edu.pe>
2020-07-19 21:36:37 -0500

    FEATURE: Finished spring basic

--- src/main/java/agileteam/ScumMaster.java
+++ src/main/java/agileteam/ScumMaster.java
@@ -5,12 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 @Component
+@Lazy(value = true)
 public class ScumMaster implements TeamMember {
 
     public static final Logger looger = LoggerFactory.getLogger(ScumMaster.class);
--- src/main/java/com/zan/dev/web/zan/dev/init/JwdInitApplication.java
+++ src/main/java/com/zan/dev/web/zan/dev/init/JwdInitApplication.java
@@ -8,14 +8,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan("agileteam")
+@PropertySource("classpath:demo.properties")
 public class JwdInitApplication implements CommandLineRunner {
 
 	private static final Logger logger = LoggerFactory.getLogger(JwdInitApplication.class);
@@ -38,12 +41,18 @@ public class JwdInitApplication implements CommandLineRunner {
 	@Qualifier("technicalLeader")
 	private TeamMember technicalLeader;
 
+	@Value("${nombre-equipo}")
+	private String teamName;
+
 	public static void main(String[] args) {
 		SpringApplication.run(JwdInitApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		logger.info("Name Team: " + teamName);
+
 		logger.info(developerFrontEnd.doAction());
 		logger.info(developerBackEnd.doReport());
 		logger.info(scumMaster.doReport());
